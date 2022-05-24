ece4094b1710ce276e5f5449dec4531ba5f1369e
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-24 17:55:08 +0200

    finish chapter 2.

--- 02-Basic-Types/app.ts
+++ 02-Basic-Types/app.ts
@@ -1,26 +1,14 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Wojtek";
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log(`Result: ${num}`);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let comibnedValues: (a: number, b: number) => number;
-
-comibnedValues = add;
-/* comibnedValues = printResult; */
-/* comibnedValues = 5; */
-
-console.log(comibnedValues(9, 9));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+generateError("Error!", 500);
